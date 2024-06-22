f6fa0f103b8bc984d116cd19bb0768bab6c83d50
adesoft75 <adesoft@mail.ru>
2024-06-22 11:15:19 +0500

    Ver 0.01

--- src/App.js
+++ src/App.js
@@ -6,19 +6,11 @@ import AppAnswers from './AppAnswers';
 
 
 function App() {
-  const questons = 
-    [
-      {id: 0, text: 'Использовать раздел', varAnswers: ['Да','Нет','Возможно'], defAnswer: 'Да'},
-      {id: 1, text: 'Выполняем сами', varAnswers: ['Да','Нет','Возможно'], defAnswer: 'Да'},
-      {id: 2, text: 'Новая система', varAnswers: ['Да','Нет','Возможно'], defAnswer: 'Нет'},
-      {id: 3, text: 'Использовать раздел', varAnswers: ['Да','Нет','Возможно'], defAnswer: 'Да'}
-    ]    
-  const [selectedIndex, setSelectedIndex] = React.useState(1)    
     return (
-      <div className="App">    
+      <div className="App">
         <AppBarMenu />
         <AppChapters />
-        <AppAnswers questons />       
+        <AppAnswers questons />
       </div>
     );
 }
