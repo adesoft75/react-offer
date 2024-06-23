27d34cf2cccee4b9ba8fa86bb6810b35b13e9b0f
adesoft75 <adesoft@mail.ru>
2024-06-23 19:59:07 +0500

    ver.0.03

--- src/AppBarMenu.js
+++ src/AppBarMenu.js
@@ -12,8 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout']
 
 function AppBarMenu() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -39,7 +38,7 @@ function AppBarMenu() {
       <Container maxWidth="xl">
         <Toolbar disableGutters>
 
-        <Typography
+        <Typography                                //заголовок - большой экран
             variant="h5"
             noWrap
             component="a"
@@ -57,44 +56,7 @@ function AppBarMenu() {
             Проект «Ы»
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-          <Typography
+          <Typography                             //заголовок - маленький экран
             variant="h5"
             noWrap
             component="a"
@@ -112,22 +74,11 @@ function AppBarMenu() {
           >
             Проект «Ы»
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, boxShadow: 5 }}>
+                <Avatar alt="Dmitriy Ananin" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
