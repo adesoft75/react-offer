c2d01a5173420ea7b2dc74b6d1e35c6ad55f9b40
adesoft75 <adesoft@mail.ru>
2024-06-24 17:17:26 +0500

    ver.0.05

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import React from 'react'
 import AppBarMenu from './AppBarMenu'
+import AppTabs from './AppTabs';
 
 
 function App() {
     return (
       <div className="App">
         <AppBarMenu />
+        <AppTabs />
       </div>
     );
 }
